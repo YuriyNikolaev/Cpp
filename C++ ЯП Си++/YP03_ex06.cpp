63146f0f48455fab01506fb1eaeae48f65f37324
YuriyNikolaev <raptor1980@mail.ru>
2022-11-18 19:12:49 +1000

    проба запуска программы из примера

--- C++ ЯП Си++/YP03_ex06.cpp
+++ C++ ЯП Си++/YP03_ex06.cpp
@@ -1,25 +1,20 @@
 // YP03_ex06.cpp -- расход мензина на 100 км пути
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
     const int evaluationPath = 100;
-    
+
     cout << "Введите количество км пути" << endl;
     double way_km = 1000;
     cin >> way_km;
-    
+
     cout << "Введите расход бензина на этот путь" << endl;
     double gas = 50;
     cin >> gas;
-    
-    
-    
-    cout << "Расход бензина на 100 км пути составил " << evaluationPath * gas / way_km << " литров";
-    
-    
 
+    cout << "Расход бензина на 100 км пути составил " << evaluationPath * gas / way_km << " литров";
 
     return 0;
 }
