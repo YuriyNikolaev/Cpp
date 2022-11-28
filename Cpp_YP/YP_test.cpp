f6c8e968b2bb763832e87215ba7bcb8a73d3fea1
YuriyNikolaev <raptor1980@mail.ru>
2022-11-28 19:53:13 +1000

    Добавил пример YP04_24.cpp

---
+++ Cpp_YP/YP_test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+  array<int, 5> ai; // создание объекта array из 5ти элементов int
+  array<double, 4> ad = {1.2, 2.1, 3.43, 4.3};
+
+  return 0;
+}
