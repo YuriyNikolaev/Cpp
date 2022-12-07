ab667b321ccacbdda6a6f540650ebe9199aff4cc
YuriyNikolaev <raptor1980@mail.ru>
2022-12-07 11:55:47 +1000

    немного обновил Tetris_v2

--- Cpp_Практика/Tetris_v2/OneLoneCoder_Tetris.cpp
+++ Cpp_Практика/Tetris_v2/OneLoneCoder_Tetris.cpp
@@ -1,3 +1,6 @@
+// взял здесь https://www.youtube.com/watch?v=8OK8_tHeCIA&list=WL&index=25&t=489s
+// на 07.12.2022 не весь код могу догнать, пока сложновато
+
 #include <iostream>
 #include <thread>
 #include <vector>
@@ -9,6 +12,7 @@ using namespace std;
 
 int nScreenWidth = 80;	// Console Screen Size X (columns)
 int nScreenHeight = 30; // Console Screen Size Y (rows)
+
 wstring tetromino[7];
 int nFieldWidth = 12;
 int nFieldHeight = 18;
@@ -20,24 +24,24 @@ int Rotate(int px, int py, int r)
 	switch (r % 4)
 	{
 	case 0:							// 0 degrees            //  0  1  2  3
-		pi = py * 4 + px; //  4  5  6  7
-		break;						//  8  9 10 11
-											// 12 13 14 15
+		pi = py * 4 + px;                                   //  4  5  6  7
+		break;						                        //  8  9 10 11
+											                // 12 13 14 15
 
-	case 1:										 // 90 degrees           // 12  8 4 0
-		pi = 12 + py - (px * 4); // 13  9 5 1
-		break;									 // 14 10 6 2
-														 // 15 11 7 3
+	case 1:							// 90 degrees           // 12  8 4 0
+		pi = 12 + py - (px * 4);                            // 13  9 5 1
+		break;									            // 14 10 6 2
+														    // 15 11 7 3
 
-	case 2:										 // 180 degrees          // 15 14 13 12
-		pi = 15 - (py * 4) - px; // 11 10  9  8
-		break;									 //  7  6  5  4
-														 //  3  2  1  0
+	case 2:							// 180 degrees          // 15 14 13 12
+		pi = 15 - (py * 4) - px;                            // 11 10  9  8
+		break;									            //  7  6  5  4
+														    //  3  2  1  0
 
-	case 3:										// 270 degrees          //  3 7 11 15
-		pi = 3 - py + (px * 4); //  2 6 10 14
-		break;									//  1 5  9 13
-														//  0 4  8 12
+	case 3:							// 270 degrees          //  3 7 11 15
+		pi = 3 - py + (px * 4);                             //  2 6 10 14
+		break;									            //  1 5  9 13
+														    //  0 4  8 12
 	}
 
 	return pi;
@@ -89,7 +93,7 @@ int main()
 	tetromino[6].append(L"..X...X..XX.....");
 
 	pField = new unsigned char[nFieldWidth * nFieldHeight]; // Create play field buffer
-	for (int x = 0; x < nFieldWidth; x++)
+	for (int x = 0; x < nFieldWidth; x++)   // Board Boundary
 		for (int y = 0; y < nFieldHeight; y++)
 			pField[y * nFieldWidth + x] = (x == 0 || x == nFieldWidth - 1 || y == nFieldHeight - 1) ? 9 : 0;
 
